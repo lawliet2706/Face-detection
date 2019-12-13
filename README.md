c3a560b617dcc0b16f6c2e7922be75bdffe7fc8c
lawliet2706 <ka4ok0061@gmail.com>
2019-12-13 14:08:45 +0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# Face detection
- 
+# Face detector
+
+Implementation of the MTCNN face detector for Keras in Python3.4+. It is based on the paper *Zhang, K et al. (2016)* [ZHANG2016]_ and MTCNN implementation of [ipazc](https://github.com/ipazc) .
+
+## Prerequisites
+* Python 3.4+
+* Keras >=2.0.0
+* OpenCV >=4.1
+
+### Installing
+
+Currently, this version of the MTCNN implementation is only supported on Python3.4 and onwards. The implementation itself can be installed through pip:
+
+    $ pip install mtcnn
+
+The implementation of MTCNN used in this script also requires OpenCV>=4.1 and Keras>=2.0.0 (and any Tensorflow supported by Keras).
+If this is the first time you use tensorflow, you will probably need to install it in your system:
+
+
+
+    $ pip install tensorflow
+
+or with `conda`
+
+
+
+    $ conda install tensorflow
+
+Note that `tensorflow-gpu` version can be used instead if a GPU device is available on the system, which will speedup the results.
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+
+## Authors
+
+See the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* **Iván de Paz Centeno** - *Initial work* - [ipazc](https://github.com/ipazc)
---
+++ facedetection.py
@@ -0,0 +1,143 @@
+# Check the directory
+
+import os
+import numpy as np
+from mtcnn import MTCNN
+import cv2
+from PIL import Image, ImageEnhance,ImageDraw, ImageFont
+
+
+names = []
+for dirname, _, filenames in os.walk('/kaggle/input/testss'):
+    for filename in filenames:
+        names.append(str(os.path.join(dirname, filename)))
+        print(os.path.join(dirname, filename))
+
+detector = MTCNN()
+
+def find_faces(names:list, mtcnn_detector,dimensions : tuple = (256,256,3),conf_thresh:float = 0.98):
+    """
+    conf_thresh: Allows to manipulate the threshold value, so we can filter out the faces with low
+                 confidence levels
+
+    names : input image we want to perform face detection on. Shout be of the format:
+            dirname + image_name ('/kaggle/input/testss/54994.png')
+
+    mtcnn_detector : Allows to perform MTCNN Detection ->
+                        a) Detection of faces (with the confidence probability)
+                        b) Detection of keypoints (left eye, right eye, nose, mouth_left, mouth_right)
+
+    dimension:  Allows to define the shape of the output images and contact_sheet ->
+                tuple of the format (width,height,num_faces), defines the dimensions of contact sheet:
+                                     width : width of one image i.e face
+                                     height : height of one image i.e face
+                                     num_face : number of faces in one row of contact sheet
+                Overall size of the concact sheet is :
+                                    overall_width  : width * num_faces
+                                    overall_height : height * int(np.ceil(faces/num_faces)), where faces is the total
+                                    number of faces detected by our detector.
+                                    So, we get overall_width by overall_height contact sheet.
+    """
+    width,height,num_faces = dimensions
+
+    if type(names) == list:
+        contact_sheets = []
+        for img_name in names:
+            # read the image and convert it from BGR to RGB
+            img = cv2.cvtColor(cv2.imread(img_name), cv2.COLOR_BGR2RGB)
+            pil_img=Image.fromarray(img)
+
+            #detect faces
+            boxes = mtcnn_detector.detect_faces(img)
+
+            faces = []
+            conf = []
+            thresh = conf_thresh
+
+            for face in boxes:
+                if face['confidence'] >=thresh:
+                    faces.append(face['box'])
+                    conf.append(face['confidence'])
+            i = 0
+            faces_in_each = []
+
+            for x,y,w,h in faces:
+                cropped = pil_img.crop((x-0.3*w,y-0.3*h,x+w+0.3*w,y+h+0.3*h)).resize((width,height))
+                d = ImageDraw.Draw(cropped)
+                s = str('Confidence level: ' + str(conf[i]))
+                d.text((30,30), s, fill=(255,255,0))
+                faces_in_each.append(cropped)
+                i+=1
+                #modifying local data structure in each iteration to sotre PIL Image of each face
+                #display((pil_img.crop((x,y,x+w,y+h))).resize((110,110)))
+
+                contact_sheet = Image.new(pil_img.mode, (width*num_faces,height*int(np.ceil(len(faces_in_each)/num_faces))))
+                #contact sheet modification to display each iteration's result
+                x = 0
+                y = 0
+
+                for face in faces_in_each:
+                    face.thumbnail((width,height))
+                    contact_sheet.paste(face, (x, y))
+                    if x+width == contact_sheet.width:
+                        x=0
+                        y=y+height
+                    else:
+                        x=x+width
+            print('The dimensions of the original image: ')
+            print('width:',pil_img.size[0], '  height:',pil_img.size[1])
+
+            if num_faces>1:
+                display((pil_img).resize((width*num_faces-1,width*num_faces-1)))
+            else:
+                display((pil_img).resize((width*num_faces,width*num_faces)))
+            contact_sheets.append(contact_sheet)
+        return contact_sheets
+    else:
+        # read the image and convert it from BGR to RGB
+        img = cv2.cvtColor(cv2.imread(names), cv2.COLOR_BGR2RGB)
+        pil_img=Image.fromarray(img)
+
+        boxes = mtcnn_detector.detect_faces(img)
+        faces = []
+        conf = []
+        thresh = conf_thresh
+        for face in boxes:
+            if face['confidence'] >=thresh:
+                faces.append(face['box'])
+                conf.append(face['confidence'])
+        i = 0
+        faces_in_each = []
+        for x,y,w,h in faces:
+            cropped = pil_img.crop((x-0.3*w,y-0.3*h,x+w+0.3*w,y+h+0.3*h)).resize((width,height))
+            d = ImageDraw.Draw(cropped)
+            s = str('Confidence level: ' + str(conf[i]))
+            d.text((30,30), s, fill=(255,255,0))
+            faces_in_each.append(cropped)
+            i+=1
+            #modifying local data structure in each iteration to sotre PIL Image of each face
+            #display((pil_img.crop((x,y,x+w,y+h))).resize((110,110)))
+
+            contact_sheet = Image.new(pil_img.mode, (width*num_faces,height*int(np.ceil(len(faces_in_each)/num_faces))))
+            #contact sheet modification to display each iteration's result
+            x = 0
+            y = 0
+
+            for face in faces_in_each:
+                face.thumbnail((width,height))
+                contact_sheet.paste(face, (x, y))
+                if x+width == contact_sheet.width:
+                    x=0
+                    y=y+height
+                else:
+                    x=x+width
+        print('The dimensions of the original image: ')
+        print('width:',pil_img.size[0], '  height:',pil_img.size[1])
+        if num_faces>1:
+            display((pil_img).resize((width*num_faces-1,width*num_faces-1)))
+        else:
+            display((pil_img).resize((width*num_faces,width*num_faces)))
+        return contact_sheet
+
+a = find_faces(names[5],detector)
+display(a)
