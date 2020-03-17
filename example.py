9e567ae4077a062fe0d777449a68229c0662c713
Nursultan Bekmuratov <ka4ok0061@gmail.com>
2020-03-17 15:56:02 +0600

    asd
    asd

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from mtcnn import MTCNN
 
 detector = MTCNN(steps_threshold  = [0.5, 0.7, 0.98])
 
-image = 'hoomens.jpg'
+image = '1111.jpg'
 
 result = find_faces(image,detector)
 for img in result:
